523c826b248f9349f99c96b7123a538214e5ffd6
Replit user <>
2023-03-30 16:27:39 +0000

    new try

--- arraylist.c
+++ arraylist.c
@@ -47,12 +47,12 @@ void push(ArrayList * l, void * data, int i){
 }
 
 void* pop(ArrayList * l, int i){
-  void **datos = realloc(l->data,sizeof(*l->data));
-  for (i = l->capacity; i > 0; i--){
-    datos[i] = datos[i-1];
+  
+  for (j = l->size; j < i; j--){
+    l->data[j] = l->data[i-1];
     if (i < 0) return l->data[i+l->size];
   }
-  return l->data[i];
+  return l->data;
 }
 
 void* get(ArrayList * l, int i){
